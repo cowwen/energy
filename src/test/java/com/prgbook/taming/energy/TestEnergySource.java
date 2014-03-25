838deaaadd016a2eefd1e15c5cb35262eb878665
cowwen <cowwen@qq.com>
2014-03-25 15:39:13 +0800

    adding map Concurrency operation exception

--- src/test/java/com/prgbook/taming/energy/TestEnergySource.java
+++ src/test/java/com/prgbook/taming/energy/TestEnergySource.java
@@ -5,20 +5,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Map;
+import java.util.concurrent.*;
 
 public class TestEnergySource{
 	private static Logger logger =
 		LoggerFactory.getLogger(TestEnergySource.class);
 
-    private static final EnergySource energy = EnergySource.create();
+
 	@Test
 	public void testRunningEnergy() throws InterruptedException{
 		logger.debug("Ready come to test Energy.");
-
+        final EnergySource energy = EnergySource.create();
         List<Callable<Object>> task = new ArrayList<Callable<Object>>();
 
         for(int i = 0; i < 10; i++) {
@@ -62,5 +62,51 @@ public class TestEnergySource{
         Thread.sleep(100);
         //done = true;
         logger.debug("change the done.");
+    }
+
+    private static Map<String, String> testMap = new HashMap<String, String>();
+
+    @Test
+    public void testConcurrentModification() throws InterruptedException, ExecutionException {
+        List<Callable<Object>> task = new ArrayList<Callable<Object>>();
+
+        for(int i = 0; i < 10; i++) {
+            task.add(new Callable<Object>() {
+                @Override
+                public Object call() throws Exception {
+                    for (int j = 0; j < 7; j++) {
+                        testMap.put("j" + j + " " + Thread.currentThread().getName(), j + "");
+                        Thread.sleep(1);
+                    }
+                    return null;
+                }
+            });
+        }
+
+        for(int i = 0; i < 10; i++) {
+            task.add(new Callable<Object>() {
+                @Override
+                public Object call() throws Exception {
+                    for (String key : testMap.keySet()) {
+                        key = key + key;
+                    }
+                    return null;
+                }
+            });
+        }
+
+        ExecutorService service = Executors.newFixedThreadPool(100);
+        List<Future<Object>> list = service.invokeAll(task);
+
+        service.shutdown();
+
+        for (Future<Object> obj : list) {
+            obj.get();
+        }
+
+        for (String key : testMap.keySet()) {
+            logger.info(key + " " + testMap.get(key) + ".");
+        }
+
     }
 }
